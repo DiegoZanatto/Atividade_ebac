745136d7ce205fb2b7b4c7f67ef9124e3d483e72
DiegoZanatto <diegozanatto94@hotmail.com>
2023-10-31 14:14:56 +0000

    arquivo criado

--- gasolina.py
+++ gasolina.py
@@ -1,31 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import csv
 
-dados = [
-    ["dia", "venda"],
-    [1, 5.11],
-    [2, 4.99],
-    [3, 5.02],
-    [4, 5.21],
-    [5, 5.07],
-    [6, 5.09],
-    [7, 5.13],
-    [8, 5.12],
-    [9, 4.94],
-    [10, 5.03]
-]
+df = pd.read_csv('/content/Atividade_ebac/gasolina.csv')
+#df
+sns.lineplot(data=df, x='dia', y='venda')
 
-nome_arquivo = "gasolina.csv"
-
-with open(nome_arquivo, mode="w", newline="") as arquivo_csv:
-    escritor_csv = csv.writer(arquivo_csv)
-    escritor_csv.writerows(dados)
-
-
-
-
-gasolina_df = pd.read_csv('gasolina.csv')
-gasolina = sns.lineplot(data=gasolina_df, x="dia", y="venda")
-plt.savefig('gasolina.png', dpi=300)
+plt.savefig('grafico.png')
